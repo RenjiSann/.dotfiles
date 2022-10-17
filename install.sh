7f1c988c1eec99ec737663556f03e22a70601628
Dorian 'RenjiSann' Péron <dodo.savenay@gmail.com>
2022-10-17 03:35:22 +0200

    fix(install): changed '' in ""

--- install.sh
+++ install.sh
@@ -10,18 +10,18 @@ declare -r SHELL_INSTALL_DIR="shell_install"
 declare -r KITTY_INSTALL_DIR="kitty_install"
 
 # Install i3 config
-ask_then_perform "Install i3 ?" 'cd ${I3_INSTALL_DIR} && ./install.sh && cd ..'
+ask_then_perform "Install i3 ?" "cd ${I3_INSTALL_DIR} && ./install.sh && cd .."
 
 # Install shell config
-ask_then_perform "Install shell config ?" 'cd ${SHELL_INSTALL_DIR} && ./install.sh && cd ..'
+ask_then_perform "Install shell config ?" "cd ${SHELL_INSTALL_DIR} && ./install.sh && cd .."
 
 # Install kitty config
-ask_then_perform "Install Kitty ?" 'cd ${KITTY_INSTALL_DIR} && ./install.sh && cd ..'
+ask_then_perform "Install Kitty ?" "cd ${KITTY_INSTALL_DIR} && ./install.sh && cd .."
 
 # Install vim config
-ask_then_perform "Install Vim ?" 'cd ${VIM_INSTALL_DIR} && ./install.sh && cd ..'
+ask_then_perform "Install Vim ?" "cd ${VIM_INSTALL_DIR} && ./install.sh && cd .."
 
-ask_then_perform "Install libinput-gesture config ?" 'ln -sfv -T "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf'
+ask_then_perform "Install libinput-gesture config ?" "ln -sfv -T \"$(pwd)/libinput-gestures.conf\" ~/.config/libinput-gestures.conf"
 
 echo "===> Installing .xprofile"
 create_symlink "$(pwd)/xprofile" "${HOME}/.xprofile"

876d62434aa799b56b3e9cb10d62aefb88cd9179
Dorian Peron <peron@adacore.com>
2025-06-13 17:39:07 +0200

    sss

--- sway-laptop/link_dotfiles.sh
+++ sway-laptop/link_dotfiles.sh
@@ -3,3 +3,4 @@ ln -s $(pwd)/dot_config/swaylock $HOME/.config/
 ln -s $(pwd)/dot_config/waybar $HOME/.config/
 ln -s $(pwd)/dot_config/mako $HOME/.config/
 ln -s $(pwd)/dot_config/fuzzel $HOME/.config/
+ln -s $(pwd)/dot_config/environment.d $HOME/.config/
